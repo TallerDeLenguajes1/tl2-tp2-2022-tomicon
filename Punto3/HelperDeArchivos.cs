115bf6a6cbba4e1a6d86b6e964c8034ea9f7dc42
tomicon <tomicon01@gmail.com>
2022-10-13 17:50:52 -0300

    tp2 terminado

--- Punto3/HelperDeArchivos.cs
+++ Punto3/HelperDeArchivos.cs
@@ -2,17 +2,39 @@
 public static class HelperDeArchivos
 {
     static public void escribirContenido(Alumno al){
-
-        string[] deportes= {"Atletismo", "Voley", "Futbol"};
-        string nombreArchivo= deportes[al.Curso];
-        bool archivoExiste= File.Exists(nombreArchivo);
-        StreamWriter escritor = File.AppendText(nombreArchivo);
-        if (!archivoExiste)
+        NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        try
         {
-            escritor.WriteLine("ID;DNI;Nombre;Apellido;Curso");
+            string[] deportes= {"Atletismo", "Voley", "Futbol"};
+            string nombreArchivo= deportes[al.Curso];
+            bool archivoExiste= File.Exists(nombreArchivo);
+            StreamWriter escritor = File.AppendText(nombreArchivo);
+            if (!archivoExiste)
+            {
+                escritor.WriteLine("ID;DNI;Nombre;Apellido;Curso");
+            }
+            escritor.WriteLine(al.lineaCSV());
+            escritor.Close();
         }
-        escritor.WriteLine(al.lineaCSV());
-        escritor.Close();
+        catch (IndexOutOfRangeException e)
+        {
+            logger.Error("Error: " + e.Message + "stack trace: " + e.StackTrace);
+            System.Console.WriteLine("Se quiere acceder a un elemento del arreglo que no existe. Verifique que el atributo curso en la clase Alumno tenga los valores adecuados");
+        }
+        catch(NullReferenceException e)
+        {
+            logger.Error("Error: " + e.Message + "stack trace: " + e.StackTrace);
+            System.Console.WriteLine("Error: uno de los campos necesarios contiene un null");
+        }
+        catch (System.Exception e)
+        {
+            logger.Error("Error: " + e.Message + "stack trace: " + e.StackTrace);
+            System.Console.WriteLine("ha ocurrido un error");
+        }
     }
-    
+
+    static public void borrarContenido(string path)
+    {
+        if (Path.GetFileName(path).Split('.')[1] == "csv") File.Delete(path);        
+    }
 }
--- Punto3/Program.cs
+++ Punto3/Program.cs
@@ -1,5 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using NLog;
 Console.WriteLine("Hello, World!");
 
-Alumno alum = new Alumno(1, "tomi", "capo", 43567472, 0);
-HelperDeArchivos.escribirContenido(alum);
+Logger logger = LogManager.GetCurrentClassLogger();
+System.Console.WriteLine("Cuantos alumnos quiere ingresar?");
+int cantAlumnos= Convert.ToInt32(Console.ReadLine());
+List<Alumno> listaAlumnos= new List<Alumno>();
+int id, dni, curso;
+string apellido, nombre;
+for (int i = 0; i < cantAlumnos; i++)
+{
+    try
+    {
+        System.Console.WriteLine($"Alumno nro{i+1}");
+        System.Console.WriteLine("Dni: ");
+        dni= Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Id: ");
+        id= Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine("Nombre: ");
+        nombre= Console.ReadLine();
+        System.Console.WriteLine("Apellido:");
+        apellido= Console.ReadLine();
+        System.Console.WriteLine("Curso:");
+        curso= Convert.ToInt32(Console.ReadLine());
+        Alumno alum = new Alumno(id, nombre, apellido, dni, curso);
+        listaAlumnos.Add(alum);
+    }
+    catch (NullReferenceException e) 
+    {
+        logger.Error($"Error: {e.Message} stack trace: {e.StackTrace}");
+    }
+    finally
+    {
+        logger.Info("Se termino la carga de datos");
+    }
+
+}
+foreach (var alumno in listaAlumnos)
+{
+    HelperDeArchivos.escribirContenido(alumno);    
+}
